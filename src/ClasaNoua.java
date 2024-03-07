35f66bc584758ca756e59087dcc8988752ed7061
claudiacaragea <carageaclaudiam@gmail.com>
2024-03-07 21:29:46 +0200

    primitive

---
+++ src/ClasaNoua.java
@@ -0,0 +1,7 @@
+package learning ;
+
+public class ClasaNoua {
+    public static void main(String[] args) {
+        System.out.println(" Hello Skillbrain");
+    }
+}
---
+++ src/Primitive.java
@@ -0,0 +1,22 @@
+package learning;
+public class Primitive {
+    public static void mai(String[] args) {
+
+        int numar;
+        numar = 5;
+
+        int numar2 = 66465776;
+
+        float numarCuVirgula = 7.3f;
+        double numarCuPunct = 8.5;
+
+        char unSingurCaracter = 'G';
+        long numarFoarteMare = 127642964274629;
+
+        short numarMaiMic = (short) 356223;
+        boolean trueSauFalse = true;
+        boolean trueSauFalse = false;
+
+        byte valoareMax = -126;
+    }
+}
